746e4068be273db4688a56c0c2b08d753c69581c
renato-chagas <renatochagas.m@gmail.com>
2025-11-23 19:55:26 -0300

    Refactor nexus_inventory app structure: update app name, remove unused files, and add init files

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core.nexus_inventory",
 ]
 
 MIDDLEWARE = [
--- src/core/nexus_inventory/apps.py
+++ src/core/nexus_inventory/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NexusInventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nexus_inventory'
+    name = 'core.nexus_inventory'
