d08ea98ecea6097885c70d61a6666bdafd59e1d1
renato-chagas <renatochagas.m@gmail.com>
2025-11-26 10:50:28 -0300

    style: Standardize quotes in CSRF_TRUSTED_ORIGINS settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -30,9 +30,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:3000',
 ]
 
 # Application definition
