81c1285e8906737c27677d8fc7bd8016d64da7b1
renato-chagas <renatochagas.m@gmail.com>
2025-11-26 10:51:29 -0300

    feat: Add JWT authentication and permission classes to REST framework settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -116,7 +116,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # REST Framework Configuration
 
 REST_FRAMEWORK = {
-
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
